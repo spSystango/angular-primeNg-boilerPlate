d6d0e40e33f2ada754b1cca5f2f71c890e2e689d
sukhdev Pawar <sukhdev.mmfinfotech@gmail.com>
2022-02-23 14:13:45 +0530

    feature: added auth module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',loadChildren:()=>import('./all-modules/pages/pages.module').then((m)=>m.PagesModule)}
+  {path:'',loadChildren:()=>import('./all-modules/pages/pages.module').then((m)=>m.PagesModule)},
+  {path:'auth',loadChildren:()=>import('./all-modules/auth/auth.module').then((m)=>m.AuthModule)}
 ];
 
 @NgModule({
